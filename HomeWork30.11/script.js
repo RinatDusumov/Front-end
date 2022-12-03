9f1e42956904bcbbf7d4f7172f02e8492218e40e
Rinat Dusumov <rinat_dusumow@int.pl>
2022-12-03 20:36:35 +0100

    Update script.js

--- HomeWork30.11/script.js
+++ HomeWork30.11/script.js
@@ -74,27 +74,48 @@ for (let elem of div) {
     в большем размере. 
 */
 
-let img = ['https://th.bing.com/th/id/R.0c1e52d97899a46964f85ae3a901effc?rik=Y7aM3d1OO2iJzg&riu=http%3a%2f%2fdrasler.ru%2fwp-content%2fuploads%2f2018%2f06%2f%d0%9a%d1%80%d0%b0%d1%81%d0%b8%d0%b2%d1%8b%d0%b5-%d0%b8-%d0%bf%d1%80%d0%b8%d0%ba%d0%be%d0%bb%d1%8c%d0%bd%d1%8b%d0%b5-%d0%ba%d0%b0%d1%80%d1%82%d0%b8%d0%bd%d0%ba%d0%b8-%d0%bf%d1%82%d0%b8%d1%86-%d0%bd%d0%b0-%d0%b7%d0%b0%d1%81%d1%82%d0%b0%d0%b2%d0%ba%d1%83-%d1%82%d0%b5%d0%bb%d0%b5%d1%84%d0%be%d0%bd%d0%b0-%d1%81%d0%b1%d0%be%d1%80%d0%ba%d0%b0-22.jpg&ehk=a%2b6DpbcpfHBiDMwJaxuVWm7xscoCZITu0lS4%2bDzBZeQ%3d&risl=&pid=ImgRaw&r=0', 'https://live.staticflickr.com/5213/5441565397_a2319a6edc_n.jpg', 'https://th.bing.com/th/id/OIP.bTqNQVJSEqaA0EO5PM-gHwAAAA?pid=ImgDet&rs=1'];
+let img = ['https://th.bing.com/th/id/R.0c1e52d97899a46964f85ae3a901effc?rik=Y7aM3d1OO2iJzg&riu=http%3a%2f%2fdrasler.ru%2fwp-content%2fuploads%2f2018%2f06%2f%d0%9a%d1%80%d0%b0%d1%81%d0%b8%d0%b2%d1%8b%d0%b5-%d0%b8-%d0%bf%d1%80%d0%b8%d0%ba%d0%be%d0%bb%d1%8c%d0%bd%d1%8b%d0%b5-%d0%ba%d0%b0%d1%80%d1%82%d0%b8%d0%bd%d0%ba%d0%b8-%d0%bf%d1%82%d0%b8%d1%86-%d0%bd%d0%b0-%d0%b7%d0%b0%d1%81%d1%82%d0%b0%d0%b2%d0%ba%d1%83-%d1%82%d0%b5%d0%bb%d0%b5%d1%84%d0%be%d0%bd%d0%b0-%d1%81%d0%b1%d0%be%d1%80%d0%ba%d0%b0-22.jpg&ehk=a%2b6DpbcpfHBiDMwJaxuVWm7xscoCZITu0lS4%2bDzBZeQ%3d&risl=&pid=ImgRaw&r=0', 'https://live.staticflickr.com/5213/5441565397_a2319a6edc_n.jpg', 'https://th.bing.com/th/id/OIP.bTqNQVJSEqaA0EO5PM-gHwAAAA?pid=ImgDet&rs=1', 'https://th.bing.com/th/id/OIP.Z2KrjpV1_bdkLKVyPQPVKQAAAA?pid=ImgDet&w=170&h=226&c=7&dpr=1,3', 'https://i.pinimg.com/236x/88/f5/94/88f594fc3b8e643b0c633153ae522b97--purple-christmas-christmas-art.jpg'];
 
 for (let i = 0; i < img.length; i++) {
+    
+
+    let div = document.createElement('div');
+    div.style.display = 'inline-block';
+    
     let imges = document.createElement('img');
+    div.append(imges);
+    
+
     imges.setAttribute('src', img[i])
+    imges.setAttribute('alt', "img");
+
     imges.style.display = "inline-block";
     imges.style.width = "50px";
     imges.style.height = "50px";
     imges.style.margin = "10px";
+    
     let script = document.querySelector('script')
-    script.before(imges);
+    script.before(div);
+
 
     imges.addEventListener("click", mouseImg);
+
     function mouseImg() {
-        imges.getAttribute('src');
-        console.log(imges);
+        let adres = imges.getAttribute('src');
+        console.log(adres);
+
+        let div = document.createElement('div');
+        div.style.paddingTop = "100px";
+
+        let script = document.querySelector('script')
+        script.before(div);
 
         let imges2 = document.createElement('img');
-        imges2 = imges;
-        imges2.style.width = "200px";
-        imges2.style.height = "200px";
+        div.append(imges2);
+
+        imges2.setAttribute('src', adres);
+        imges2.style.width = "400px";
+        imges2.style.height = "300px";
     }
 }
 
